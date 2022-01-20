ae52564f61dfaf17ab40f7f1c707fe57fbae4362
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-01-20 17:55:23 -0500

    commit tree 93ff70925521a94b26efb2c4f80946fc214fb379

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    return callback();
+  }
+
+function returnsANamedFunction(name){
+    return function did() {
+        
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
+
+
+
